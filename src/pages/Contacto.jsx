591837c1a27be714b2f529ce49a05f140938f581
FerAmbriz <fernandoambrizbarrera@gmail.com>
2025-11-23 10:55:34 -0600

    update en contecto

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -3,8 +3,18 @@ import React from 'react';
 export default function Contacto() {
   return (
     <div>
-      <h1>Contacto</h1>
-      <p>Formulario de contacto o información de contacto aquí.</p>
+      <header className="hero">
+        <h1>Contacto</h1>
+      </header>
+      <section className="section">
+        <div className="servicios-destacados">
+          <div className="servicio">
+            <h3>Correo</h3>
+            <p> plasmex@gmail.com.</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
+  
 }
